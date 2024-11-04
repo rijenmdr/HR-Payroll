61b34435a750586ca9babeea723a37cf4c12dada
ecv-rijen <rijen.maharjan@classifiedsventures.com>
2024-11-04 11:05:55 +0545

    feat: ui for the header

--- src/components/layout/dashboard/Header/index.tsx
+++ src/components/layout/dashboard/Header/index.tsx
@@ -6,7 +6,7 @@ import TitleWithBreadcrumb from './TitleWithBreadcrumb';
 
 const Header = () => {
   return (
-    <header className="flex justify-between items-center bg-background pl-4 md:pl-[0px] pr-[30px] py-4 shrink-0 gap-2 fixed top-0 right-0 left-0 md:left-[320px]">
+    <header className="flex justify-between items-center bg-background pl-4 md:pl-[0px] pr-[30px] my-4 py-4 shrink-0 gap-2 fixed top-0 right-0 left-0 md:left-[320px]">
       <div className="flex items-center gap-5">
         <SidebarTrigger className="-ml-1 block md:hidden" />
 
